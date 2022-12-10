8292cbe775588c423213725800990438cb2132b7
monysary <sary.mony@gmail.com>
2022-12-10 15:43:04 -0800

    Set up application to listen to port 3001 to populate static site

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.listen(3001, () => console.log("Application listening at http://localhost:3001"))
 
+app.get("/api/notes", (req, res) => {
+    
+})
