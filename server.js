9445388b662e5e215079e8cf74abd6574ccaa831
monysary <sary.mony@gmail.com>
2022-12-10 16:04:53 -0800

    Set up get request to populate the notes.html page

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express()
+const path = require("path")
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.listen(3001, () => console.log("Application listening at http://localhost:3001"))
 
 app.get("/api/notes", (req, res) => {
-    
+    res.sendFile(path.join(__dirname, "public", "notes.html"), (err) => 
+    err ? console.error(err) : console.log("Path successfully sent to notes.html"))
 })
