21c15214cc838d9011914a1add74ffb0c6ee23d4
monysary <sary.mony@gmail.com>
2022-12-10 16:11:55 -0800

    Set up get request to homepage using / path

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Setting up express and server tools
 const express = require("express");
 const app = express()
 const path = require("path")
@@ -6,7 +7,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.listen(3001, () => console.log("Application listening at http://localhost:3001"))
 
+// Get request to direct client to index.html page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"), (err) => 
+    err ? console.error(err) : console.log("Path successfully directed to index.html"))
+})
+
+// Get request to direct client to notes.html page
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "notes.html"), (err) => 
-    err ? console.error(err) : console.log("Path successfully sent to notes.html"))
+    err ? console.error(err) : console.log("Path successfully directed to notes.html"))
 })
