310d24aed06f7c6b988265008750a702075a7597
monysary <sary.mony@gmail.com>
2022-12-13 14:50:00 -0800

    Created routes for html and api

---
+++ routes/APIroutes.js
@@ -0,0 +1,79 @@
+const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
+
+// Function to return a random string
+const randomStr = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+}
+
+// GET request to return saved notes
+router.get("/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "..", "db", "db.json"), (err, data) => {
+        if (err) {
+            console.error("Error: ", err);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    })
+})
+
+// POST request to add new notes to the database
+router.post("/notes", (req, res) => {
+    const { title, text } = req.body;
+    
+    // Assigning the notes to a new object so we can pass in a unique ID
+    const newRequest = {
+        title,
+        text,
+        id: randomStr()
+    }
+    
+    // Updating database with newly added notes
+    fs.readFile(path.join(__dirname, "..", "db", "db.json"), (err, data) => {
+        if (err) {
+            console.error("Error: ", err);
+        } else {
+            const parseData = JSON.parse(data);
+            parseData.push(newRequest);
+            
+            // Adding new notes to database
+            fs.writeFile(path.join(__dirname, "..", "db", "db.json"), JSON.stringify(parseData), (err, data) => {
+                if (err) {
+                    console.error("Error: ", err);
+                } else {
+                    res.json(data);
+                    console.log("New notes successfully added to database");
+                }
+            });
+        }
+    })
+})
+
+// DELETE request to remove saved notes when user clicks delete next to the note
+router.delete("/notes/:id", (req, res) => {
+    fs.readFile(path.join(__dirname, "..", "db", "db.json"), (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parseData = JSON.parse(data);
+            filterData = parseData.filter((obj) => {
+                return obj.id !== req.params.id
+            });
+            
+            // Replace notes in database with removed note
+            fs.writeFile(path.join(__dirname, "..", "db", "db.json"), JSON.stringify(filterData), (err, data) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    res.json(data);
+                    console.log("Note deleted from database");
+                }
+            });
+        }
+    })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,88 +3,17 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
-const { parse } = require("path");
+
+// Setting up middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Function to return a random string
-const randomStr = () => {
-    return Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1);
-}
-
+// Setting up routing files
 const htmlRouter = require(path.join(__dirname, "routes", "HTMLroutes.js"));
-
+const apiRouter = require(path.join(__dirname, "routes", "APIroutes.js"));
+app.use("/api", apiRouter);
 app.use("/", htmlRouter);
-
-// GET request to return saved notes
-app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "db", "db.json"), (err, data) => {
-        if (err) {
-            console.error("Error: ", err);
-        } else {
-            res.json(JSON.parse(data));
-        }
-    })
-})
-
-// POST request to add new notes to the database
-app.post("/api/notes", (req, res) => {
-    const { title, text } = req.body;
-    
-    // Assigning the notes to a new object so we can pass in a unique ID
-    const newRequest = {
-        title,
-        text,
-        id: randomStr()
-    }
-    
-    // Updating database with newly added notes
-    fs.readFile(path.join(__dirname, "db", "db.json"), (err, data) => {
-        if (err) {
-            console.error("Error: ", err);
-        } else {
-            const parseData = JSON.parse(data);
-            parseData.push(newRequest);
-            
-            // Adding new notes to database
-            fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(parseData), (err, data) => {
-                if (err) {
-                    console.error("Error: ", err);
-                } else {
-                    res.json(data);
-                    console.log("New notes successfully added to database");
-                }
-            });
-        }
-    })
-})
-
-// DELETE request to remove saved notes when user clicks delete next to the note
-app.delete("/api/notes/:id", (req, res) => {
-    fs.readFile(path.join(__dirname, "db", "db.json"), (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const parseData = JSON.parse(data);
-            filterData = parseData.filter((obj) => {
-                return obj.id !== req.params.id
-            });
-            
-            // Replace notes in database with removed note
-            fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(filterData), (err, data) => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    res.json(data);
-                    console.log("Note deleted from database");
-                }
-            });
-        }
-    })
-})
 
 // Application listening on port 3001
 app.listen(3001, () => console.log("Application listening at http://localhost:3001"))
